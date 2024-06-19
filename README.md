b8dceb15b84bab0845d19805ec248e3f61ed6174
Дарья Пеньшина <darya-zamotina@yandex.ru>
2024-06-19 11:38:08 +0200

    Первая сдача

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/DaryaPenshina/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
+const placesList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
+function createCard(cardData, deleteHandler) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardImage = cardElement.querySelector('.card__image').src = cardData.link;;
+    const cardTitle = cardElement.querySelector('.card__title');
+    const deleteButton = cardElement.querySelector('.card__delete-button');
 
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+    cardTitle.textContent = cardData.name;
 // @todo: Функция удаления карточки
+    deleteButton.addEventListener('click', function() {
+        deleteHandler(cardElement);
+    });
 
+    return cardElement;
+}
 // @todo: Вывести карточки на страницу
+initialCards.forEach(cardData => {
+    const newCard = createCard(cardData, handleCardDelete);
+    placesList.appendChild(newCard);
+});
+
+function handleCardDelete(cardElement) {
+    cardElement.remove();
+
+}
